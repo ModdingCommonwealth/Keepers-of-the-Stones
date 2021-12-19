6820d972dcc4f84c4e1631dbdbe97cb1264c9b43
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2021-12-19 12:33:00 +0300

    Updated mechanics for the Staff of Energy #29
    Updated mechanics for the Staff of Energy #29

--- src/main/java/ru/minefox/keepeersofthestones/procedures/EnergyStaffUseProcedure.java
+++ src/main/java/ru/minefox/keepeersofthestones/procedures/EnergyStaffUseProcedure.java
@@ -1,63 +1,71 @@
 package ru.minefox.keepeersofthestones.procedures;
 
+import ru.minefox.keepeersofthestones.network.PowerModVariables;
 import ru.minefox.keepeersofthestones.init.PowerModMobEffects;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffectInstance;
 
 public class EnergyStaffUseProcedure {
-	public static void execute(Entity entity) {
-		if (entity == null)
+	public static void execute(Entity entity, Entity sourceentity) {
+		if (entity == null || sourceentity == null)
 			return;
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.UNELEMENTAL_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.FIRE_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.AIR_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.WATER_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.EARTH_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.ICE_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.LIGHTNING_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.FIRE_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.CRYSTAL_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.LAVA_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.RAIN_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.TORNADO_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.OCEAN_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.FIRE_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.GREENERY_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.GEYSER_MERGER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.ANIMALS_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.METAL_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.LIGHT_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.SHADOW_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.SUN_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.MOON_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.CREATION_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.DESTRUCTION_MASTER);
-		if (entity instanceof LivingEntity _entity)
-			_entity.removeEffect(PowerModMobEffects.COAL_MERGER);
+		if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).active) {
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.UNELEMENTAL_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.FIRE_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.AIR_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.WATER_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.EARTH_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.ICE_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.LIGHTNING_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.FIRE_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.CRYSTAL_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.LAVA_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.RAIN_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.TORNADO_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.OCEAN_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.FIRE_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.GREENERY_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.GEYSER_MERGER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.ANIMALS_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.METAL_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.LIGHT_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.SHADOW_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.SUN_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.MOON_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.CREATION_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.DESTRUCTION_MASTER);
+			if (entity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.COAL_MERGER);
+			if (sourceentity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(PowerModMobEffects.ENERGY_MASTER, 24000, 0, (false), (false)));
+			if (sourceentity instanceof LivingEntity _entity)
+				_entity.removeEffect(PowerModMobEffects.RECHARGE_ENERGY_STONE);
+		}
 	}
 }
