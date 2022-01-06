99f2c92cdd61aa1c9493afe39bc8b82ede845a4d
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-01-06 13:21:21 +0300

    Fix tornado behavior
    Fix tornado behavior

--- src/main/java/ru/minefox/keepeersofthestones/procedures/TameTornadoProcedure.java
+++ src/main/java/ru/minefox/keepeersofthestones/procedures/TameTornadoProcedure.java
@@ -21,32 +21,32 @@ public class TameTornadoProcedure {
 				: false) {
 			if (entity instanceof TamableAnimal _toTame && sourceentity instanceof Player _owner)
 				_toTame.tame(_owner);
-			new Object() {
-				private int ticks = 0;
-				private float waitTicks;
-				private LevelAccessor world;
-
-				public void start(LevelAccessor world, int waitTicks) {
-					this.waitTicks = waitTicks;
-					MinecraftForge.EVENT_BUS.register(this);
-					this.world = world;
-				}
-
-				@SubscribeEvent
-				public void tick(TickEvent.ServerTickEvent event) {
-					if (event.phase == TickEvent.Phase.END) {
-						this.ticks += 1;
-						if (this.ticks >= this.waitTicks)
-							run();
-					}
-				}
-
-				private void run() {
-					if (!entity.level.isClientSide())
-						entity.discard();
-					MinecraftForge.EVENT_BUS.unregister(this);
-				}
-			}.start(world, 400);
 		}
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private LevelAccessor world;
+
+			public void start(LevelAccessor world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				if (!entity.level.isClientSide())
+					entity.discard();
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, 400);
 	}
 }
