d914ba6786464de6d518c1c55924c397e0064a88
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-01-26 21:06:59 +0300

    Revert "Merge branch 'main' into 1.16.5"
    This reverts commit 06e935627e63235ab53bdeca99380d80e7719dc0, reversing
    changes made to 25253ada06d06429143290d8a7626543c447ac16.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Keepers-of-the-Stones
-<br>Minecraft Support: 1.17.1
-<br>Mod Version: 1.2.1 Release
-<br>Support type: Main
+<br>Minecraft Support: 1.16.5
+<br>Mod Version: 1.2.0 LTS
+<br>Support type: LTS (until 01.03.2022)
--- src/main/java/power/keepeersofthestones/client/gui/ChoiseMagicStonesPage2Screen.java
+++ src/main/java/power/keepeersofthestones/client/gui/ChoiseMagicStonesPage2Screen.java
@@ -63,6 +63,9 @@ public class ChoiseMagicStonesPage2Screen extends AbstractContainerScreen<Choise
 		RenderSystem.setShaderTexture(0, new ResourceLocation("power:textures/cosmos_master.png"));
 		this.blit(ms, this.leftPos + 19, this.topPos + 114, 0, 0, 16, 16, 16, 16);
 
+		RenderSystem.setShaderTexture(0, new ResourceLocation("power:textures/blood_master.png"));
+		this.blit(ms, this.leftPos + 19, this.topPos + 141, 0, 0, 16, 16, 16, 16);
+
 		RenderSystem.disableBlend();
 	}
 
@@ -119,7 +122,11 @@ public class ChoiseMagicStonesPage2Screen extends AbstractContainerScreen<Choise
 				ChoiseMagicStonesPage2ButtonMessage.handleButtonAction(entity, 3, x, y, z);
 			}
 		}));
-		this.addRenderableWidget(new Button(this.leftPos + 37, this.topPos + 141, 56, 20, new TextComponent(" "), e -> {
+		this.addRenderableWidget(new Button(this.leftPos + 37, this.topPos + 141, 56, 20, new TextComponent(" Blood"), e -> {
+			if (true) {
+				PowerMod.PACKET_HANDLER.sendToServer(new ChoiseMagicStonesPage2ButtonMessage(4, x, y, z));
+				ChoiseMagicStonesPage2ButtonMessage.handleButtonAction(entity, 4, x, y, z);
+			}
 		}));
 		this.addRenderableWidget(new Button(this.leftPos + 136, this.topPos + 33, 56, 20, new TextComponent(" "), e -> {
 		}));
