6c4aae74270b9792913834e023987775e31ca4fa
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-02-15 11:40:55 +0300

    Fixed a bug with a poisonous thorn
    Fixed a bug with a poisonous thorn

--- src/main/java/power/keepeersofthestones/procedures/SpikePoisonProcedureProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/SpikePoisonProcedureProcedure.java
@@ -1,9 +1,8 @@
 package power.keepeersofthestones.procedures;
 
-import power.keepeersofthestones.init.PowerModMobEffects;
-
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 
 public class SpikePoisonProcedureProcedure {
@@ -11,6 +10,6 @@ public class SpikePoisonProcedureProcedure {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(PowerModMobEffects.FIRE_MASTER, 100, 2));
+			_entity.addEffect(new MobEffectInstance(MobEffects.POISON, 100, 2));
 	}
 }
