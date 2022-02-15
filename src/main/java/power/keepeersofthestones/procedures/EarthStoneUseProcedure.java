4753c01980437d1542d9b80d2bf3d8e212bad713
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-02-15 14:59:29 +0300

    Fixed a bug with a dup of stones in the left hand
    Fixed a bug with a dup of stones in the left hand

--- src/main/java/power/keepeersofthestones/procedures/EarthStoneUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/EarthStoneUseProcedure.java
@@ -2,7 +2,9 @@ package power.keepeersofthestones.procedures;
 
 import power.keepeersofthestones.network.PowerModVariables;
 import power.keepeersofthestones.init.PowerModMobEffects;
+import power.keepeersofthestones.init.PowerModItems;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -11,10 +13,13 @@ public class EarthStoneUseProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (!(entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(PowerModMobEffects.RECHARGE_EARTH_STONE) : false)) {
-			if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).active) {
-				if (entity instanceof LivingEntity _entity)
-					_entity.addEffect(new MobEffectInstance(PowerModMobEffects.EARTH_MASTER, 12000, 0, (false), (false)));
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PowerModItems.EARTH_STONE) {
+			if (!(entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(PowerModMobEffects.RECHARGE_EARTH_STONE) : false)) {
+				if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+						.orElse(new PowerModVariables.PlayerVariables())).active) {
+					if (entity instanceof LivingEntity _entity)
+						_entity.addEffect(new MobEffectInstance(PowerModMobEffects.EARTH_MASTER, 12000, 0, (false), (false)));
+				}
 			}
 		}
 	}
--- src/main/java/power/keepeersofthestones/procedures/SunStoneUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/SunStoneUseProcedure.java
@@ -2,7 +2,9 @@ package power.keepeersofthestones.procedures;
 
 import power.keepeersofthestones.network.PowerModVariables;
 import power.keepeersofthestones.init.PowerModMobEffects;
+import power.keepeersofthestones.init.PowerModItems;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -11,10 +13,12 @@ public class SunStoneUseProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).active) {
-			if (!(entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(PowerModMobEffects.RECHARGE_SUN_STONE) : false)) {
-				if (entity instanceof LivingEntity _entity)
-					_entity.addEffect(new MobEffectInstance(PowerModMobEffects.SUN_MASTER, 12000, 0, (false), (false)));
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PowerModItems.SUN_STONE) {
+			if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).active) {
+				if (!(entity instanceof LivingEntity _livEnt ? _livEnt.hasEffect(PowerModMobEffects.RECHARGE_SUN_STONE) : false)) {
+					if (entity instanceof LivingEntity _entity)
+						_entity.addEffect(new MobEffectInstance(PowerModMobEffects.SUN_MASTER, 12000, 0, (false), (false)));
+				}
 			}
 		}
 	}
