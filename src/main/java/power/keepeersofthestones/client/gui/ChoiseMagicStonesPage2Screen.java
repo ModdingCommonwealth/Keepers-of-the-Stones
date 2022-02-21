8d4f445cff9155bbd78fcb710b700e567401df37
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-02-21 13:26:36 +0300

    Technology added in choise stones

--- src/main/java/power/keepeersofthestones/client/gui/ChoiseMagicStonesPage2Screen.java
+++ src/main/java/power/keepeersofthestones/client/gui/ChoiseMagicStonesPage2Screen.java
@@ -69,6 +69,9 @@ public class ChoiseMagicStonesPage2Screen extends AbstractContainerScreen<Choise
 		RenderSystem.setShaderTexture(0, new ResourceLocation("power:textures/time_master.png"));
 		this.blit(ms, this.leftPos + 118, this.topPos + 33, 0, 0, 16, 16, 16, 16);
 
+		RenderSystem.setShaderTexture(0, new ResourceLocation("power:textures/technology_master.png"));
+		this.blit(ms, this.leftPos + 118, this.topPos + 60, 0, 0, 16, 16, 16, 16);
+
 		RenderSystem.disableBlend();
 	}
 
@@ -137,7 +140,7 @@ public class ChoiseMagicStonesPage2Screen extends AbstractContainerScreen<Choise
 				ChoiseMagicStonesPage2ButtonMessage.handleButtonAction(entity, 5, x, y, z);
 			}
 		}));
-		this.addRenderableWidget(new Button(this.leftPos + 136, this.topPos + 60, 56, 20, new TextComponent(" "), e -> {
+		this.addRenderableWidget(new Button(this.leftPos + 136, this.topPos + 60, 56, 20, new TextComponent("Technology "), e -> {
 		}));
 		this.addRenderableWidget(new Button(this.leftPos + 136, this.topPos + 87, 56, 20, new TextComponent(" "), e -> {
 		}));
