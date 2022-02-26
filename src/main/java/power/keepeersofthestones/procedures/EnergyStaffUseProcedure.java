8515be2d1708d3fa45b8fc419e35806953053f74
Алексей Чижиков <animif220@gmail.com>
2022-02-27 00:04:51 +0300

    Refactor and fix some recharge mechanics spells #82

--- src/main/java/power/keepeersofthestones/procedures/EnergyStaffUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/EnergyStaffUseProcedure.java
@@ -14,7 +14,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.client.Minecraft;
 
 public class EnergyStaffUseProcedure {
 	public static void execute(LevelAccessor world, Entity entity, Entity sourceentity, ItemStack itemstack) {
@@ -23,48 +22,6 @@ public class EnergyStaffUseProcedure {
 		if ((entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new PowerModVariables.PlayerVariables())).active) {
 			if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
 					.getItem() == PowerModItems.ENERGY_STAFF) {
-				if (sourceentity instanceof Player _player)
-					_player.getCooldowns().addCooldown(itemstack.getItem(), 800);
-				if (world.isClientSide())
-					Minecraft.getInstance().gameRenderer.displayItemActivation(itemstack);
-				{
-					boolean _setval = (boolean) (true);
-					sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.recharge_spell_energy = _setval;
-						capability.syncPlayerVariables(sourceentity);
-					});
-				}
-				new Object() {
-					private int ticks = 0;
-					private float waitTicks;
-					private LevelAccessor world;
-
-					public void start(LevelAccessor world, int waitTicks) {
-						this.waitTicks = waitTicks;
-						MinecraftForge.EVENT_BUS.register(this);
-						this.world = world;
-					}
-
-					@SubscribeEvent
-					public void tick(TickEvent.ServerTickEvent event) {
-						if (event.phase == TickEvent.Phase.END) {
-							this.ticks += 1;
-							if (this.ticks >= this.waitTicks)
-								run();
-						}
-					}
-
-					private void run() {
-						{
-							boolean _setval = (boolean) (false);
-							sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-								capability.recharge_spell_energy = _setval;
-								capability.syncPlayerVariables(sourceentity);
-							});
-						}
-						MinecraftForge.EVENT_BUS.unregister(this);
-					}
-				}.start(world, 800);
 				if (!(entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
 						.orElse(new PowerModVariables.PlayerVariables())).recharge_spell_energy) {
 					if (entity instanceof LivingEntity _entity)
@@ -122,6 +79,46 @@ public class EnergyStaffUseProcedure {
 						_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1,
 								_player.inventoryMenu.getCraftSlots());
 					}
+					if (sourceentity instanceof Player _player)
+						_player.getCooldowns().addCooldown(itemstack.getItem(), 800);
+					{
+						boolean _setval = (boolean) (true);
+						sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+							capability.recharge_spell_energy = _setval;
+							capability.syncPlayerVariables(sourceentity);
+						});
+					}
+					new Object() {
+						private int ticks = 0;
+						private float waitTicks;
+						private LevelAccessor world;
+
+						public void start(LevelAccessor world, int waitTicks) {
+							this.waitTicks = waitTicks;
+							MinecraftForge.EVENT_BUS.register(this);
+							this.world = world;
+						}
+
+						@SubscribeEvent
+						public void tick(TickEvent.ServerTickEvent event) {
+							if (event.phase == TickEvent.Phase.END) {
+								this.ticks += 1;
+								if (this.ticks >= this.waitTicks)
+									run();
+							}
+						}
+
+						private void run() {
+							{
+								boolean _setval = (boolean) (false);
+								sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+									capability.recharge_spell_energy = _setval;
+									capability.syncPlayerVariables(sourceentity);
+								});
+							}
+							MinecraftForge.EVENT_BUS.unregister(this);
+						}
+					}.start(world, 800);
 				}
 			}
 		}
--- src/main/java/power/keepeersofthestones/procedures/MoonWitherUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/MoonWitherUseProcedure.java
@@ -14,59 +14,56 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.client.Minecraft;
 
 public class MoonWitherUseProcedure {
 	public static void execute(LevelAccessor world, Entity entity, Entity sourceentity, ItemStack itemstack) {
 		if (entity == null || sourceentity == null)
 			return;
 		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PowerModItems.MOON_WITHER) {
-			if (world.isClientSide())
-				Minecraft.getInstance().gameRenderer.displayItemActivation(itemstack);
-			if (sourceentity instanceof Player _player)
-				_player.getCooldowns().addCooldown(itemstack.getItem(), 400);
-			{
-				boolean _setval = (boolean) (true);
-				sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-					capability.recharge_spell_moon = _setval;
-					capability.syncPlayerVariables(sourceentity);
-				});
-			}
-			new Object() {
-				private int ticks = 0;
-				private float waitTicks;
-				private LevelAccessor world;
-
-				public void start(LevelAccessor world, int waitTicks) {
-					this.waitTicks = waitTicks;
-					MinecraftForge.EVENT_BUS.register(this);
-					this.world = world;
-				}
-
-				@SubscribeEvent
-				public void tick(TickEvent.ServerTickEvent event) {
-					if (event.phase == TickEvent.Phase.END) {
-						this.ticks += 1;
-						if (this.ticks >= this.waitTicks)
-							run();
-					}
-				}
-
-				private void run() {
-					{
-						boolean _setval = (boolean) (false);
-						sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-							capability.recharge_spell_moon = _setval;
-							capability.syncPlayerVariables(sourceentity);
-						});
-					}
-					MinecraftForge.EVENT_BUS.unregister(this);
-				}
-			}.start(world, 400);
 			if (!(sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
 					.orElse(new PowerModVariables.PlayerVariables())).recharge_spell_moon) {
+				if (sourceentity instanceof Player _player)
+					_player.getCooldowns().addCooldown(itemstack.getItem(), 400);
 				if (entity instanceof LivingEntity _entity)
 					_entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 200, 4, (false), (false)));
+				{
+					boolean _setval = (boolean) (true);
+					sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+						capability.recharge_spell_moon = _setval;
+						capability.syncPlayerVariables(sourceentity);
+					});
+				}
+				new Object() {
+					private int ticks = 0;
+					private float waitTicks;
+					private LevelAccessor world;
+
+					public void start(LevelAccessor world, int waitTicks) {
+						this.waitTicks = waitTicks;
+						MinecraftForge.EVENT_BUS.register(this);
+						this.world = world;
+					}
+
+					@SubscribeEvent
+					public void tick(TickEvent.ServerTickEvent event) {
+						if (event.phase == TickEvent.Phase.END) {
+							this.ticks += 1;
+							if (this.ticks >= this.waitTicks)
+								run();
+						}
+					}
+
+					private void run() {
+						{
+							boolean _setval = (boolean) (false);
+							sourceentity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+								capability.recharge_spell_moon = _setval;
+								capability.syncPlayerVariables(sourceentity);
+							});
+						}
+						MinecraftForge.EVENT_BUS.unregister(this);
+					}
+				}.start(world, 400);
 			}
 		}
 	}
