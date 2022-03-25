cb58751a5268b41328af7dbc9a83495c22b30667
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-03-25 19:02:15 +0300

    Fixed a bug due to which the vacuum network and the black hole could not be placed in the world

--- src/main/java/power/keepeersofthestones/procedures/VacuumWebUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/VacuumWebUseProcedure.java
@@ -1,17 +1,12 @@
 package power.keepeersofthestones.procedures;
 
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vec2;
+import power.keepeersofthestones.init.PowerModBlocks;
+
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.CommandSource;
+import net.minecraft.core.BlockPos;
 
 public class VacuumWebUseProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (world instanceof ServerLevel _level)
-			_level.getServer().getCommands().performCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "",
-					new TextComponent(""), _level.getServer(), null).withSuppressedOutput(), "fill ~ ~ ~ ~ ~ ~ power:vacuum_web_block replace air");
+		world.setBlock(new BlockPos((int) x, (int) (y + 1), (int) z), PowerModBlocks.VACUUM_WEB_BLOCK.get().defaultBlockState(), 3);
 	}
 }
