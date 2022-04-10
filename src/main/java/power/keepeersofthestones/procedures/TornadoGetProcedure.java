2238f7758d359610b6b2632b9897edd618b05a08
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-04-10 18:38:54 +0300

    Added the Mighty Master achievement

--- src/main/java/power/keepeersofthestones/procedures/TornadoGetProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/TornadoGetProcedure.java
@@ -9,6 +9,12 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import java.util.Iterator;
 
 public class TornadoGetProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
@@ -32,6 +38,15 @@ public class TornadoGetProcedure {
 				}
 				if (entity instanceof Player _player)
 					_player.closeContainer();
+				if (entity instanceof ServerPlayer _player) {
+					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("power:mighty_master"));
+					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+					if (!_ap.isDone()) {
+						Iterator _iterator = _ap.getRemainingCriteria().iterator();
+						while (_iterator.hasNext())
+							_player.getAdvancements().award(_adv, (String) _iterator.next());
+					}
+				}
 			}
 		}
 	}
