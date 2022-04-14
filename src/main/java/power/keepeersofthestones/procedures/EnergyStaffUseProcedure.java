0ecd59b4437caa8512108ac72d94e2ccd58b12ad
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-04-14 15:19:51 +0300

    Added several new achievements

--- src/main/java/power/keepeersofthestones/procedures/EnergyStaffUseProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/EnergyStaffUseProcedure.java
@@ -14,6 +14,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import java.util.Iterator;
 
 public class EnergyStaffUseProcedure {
 	public static void execute(LevelAccessor world, Entity entity, Entity sourceentity, ItemStack itemstack) {
@@ -86,6 +92,15 @@ public class EnergyStaffUseProcedure {
 						ItemStack _stktoremove = new ItemStack(PowerModItems.ENERGY_STONE.get());
 						_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1,
 								_player.inventoryMenu.getCraftSlots());
+					}
+					if (sourceentity instanceof ServerPlayer _player) {
+						Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("power:powerless"));
+						AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+						if (!_ap.isDone()) {
+							Iterator _iterator = _ap.getRemainingCriteria().iterator();
+							while (_iterator.hasNext())
+								_player.getAdvancements().award(_adv, (String) _iterator.next());
+						}
 					}
 				}
 				{
