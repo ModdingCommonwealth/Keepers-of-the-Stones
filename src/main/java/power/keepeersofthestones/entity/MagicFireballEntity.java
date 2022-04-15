eaff3e3118ee3f962c158c00c74115850dae0169
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-04-15 22:04:34 +0300

    Fixed a bug where a player could burn after using a magic fireball

--- src/main/java/power/keepeersofthestones/entity/MagicFireballEntity.java
+++ src/main/java/power/keepeersofthestones/entity/MagicFireballEntity.java
@@ -17,7 +17,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.projectile.ItemSupplier;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.sounds.SoundSource;
@@ -64,12 +63,6 @@ public class MagicFireballEntity extends AbstractArrow implements ItemSupplier {
 	protected void doPostHurtEffects(LivingEntity entity) {
 		super.doPostHurtEffects(entity);
 		entity.setArrowCount(entity.getArrowCount() - 1);
-	}
-
-	@Override
-	public void playerTouch(Player entity) {
-		super.playerTouch(entity);
-		BurnProcedureProcedure.execute(entity);
 	}
 
 	@Override
