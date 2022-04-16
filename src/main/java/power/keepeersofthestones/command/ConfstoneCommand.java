7d0fc6f89189062ee4fbc861198ae5a141a164c6
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-04-16 12:39:41 +0300

    Removed the book argument from the /confstone command

--- src/main/java/power/keepeersofthestones/command/ConfstoneCommand.java
+++ src/main/java/power/keepeersofthestones/command/ConfstoneCommand.java
@@ -2,7 +2,6 @@
 package power.keepeersofthestones.command;
 
 import power.keepeersofthestones.procedures.ResetmychoiceProcedureProcedure;
-import power.keepeersofthestones.procedures.OpenBookProcedure;
 import power.keepeersofthestones.procedures.DetransformKeyPriNazhatiiKlavishiProcedure;
 
 import org.checkerframework.checker.units.qual.s;
@@ -49,18 +48,6 @@ public class ConfstoneCommand {
 
 					DetransformKeyPriNazhatiiKlavishiProcedure.execute(entity);
 					return 0;
-				}))).then(Commands.literal("book").executes(cmdargs -> {
-					ServerLevel world = cmdargs.getSource().getLevel();
-					double x = cmdargs.getSource().getPosition().x();
-					double y = cmdargs.getSource().getPosition().y();
-					double z = cmdargs.getSource().getPosition().z();
-					Entity entity = cmdargs.getSource().getEntity();
-					Direction direction = Objects.requireNonNull(entity).getDirection();
-					if (entity == null)
-						entity = FakePlayerFactory.getMinecraft(world);
-
-					OpenBookProcedure.execute(world, x, y, z, entity);
-					return 0;
-				})));
+				}))));
 	}
 }
