dabaf06032f4315e0dd9e2e7377e97d9cf755a90
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-04-23 20:50:10 +0300

    Update mod to 2.0.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Keepers-of-the-Stones
 <br>Minecraft Support: 1.18.2
-<br>Mod Version: 2.1.0 Beta
+<br>Mod Version: 2.0.2 Release
 <br>Support type: Main
--- src/main/java/power/keepeersofthestones/init/PowerModGameRules.java
+++ src/main/java/power/keepeersofthestones/init/PowerModGameRules.java
@@ -11,7 +11,7 @@ import net.minecraft.world.level.GameRules;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PowerModGameRules {
 	public static final GameRules.Key<GameRules.BooleanValue> GETTINGSTONESWHENENTERINGTHEGAME = GameRules
-			.register("gettingStonesWhenEnteringTheGame", GameRules.Category.PLAYER, GameRules.BooleanValue.create(false));
+			.register("gettingStonesWhenEnteringTheGame", GameRules.Category.PLAYER, GameRules.BooleanValue.create(true));
 	public static final GameRules.Key<GameRules.BooleanValue> CANRECHOISESTONE = GameRules.register("canRechoiseStone", GameRules.Category.PLAYER,
-			GameRules.BooleanValue.create(false));
+			GameRules.BooleanValue.create(true));
 }
