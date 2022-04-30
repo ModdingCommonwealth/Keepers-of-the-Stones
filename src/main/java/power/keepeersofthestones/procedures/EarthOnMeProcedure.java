12a273a7515f85707ec4a4e185473da9e688eb71
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-04-30 16:12:47 +0300

    Increased time of slow fall

--- src/main/java/power/keepeersofthestones/procedures/EarthOnMeProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/EarthOnMeProcedure.java
@@ -39,6 +39,6 @@ public class EarthOnMeProcedure {
 				_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4), "tp ~ 256 ~");
 		}
 		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 400, 0, (false), (false)));
+			_entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 600, 0, (false), (false)));
 	}
 }
